8f38f22d2f6dfc67ee6d835158fa3be737d3a1c9
Kyle Samuel <Ksamuel92@gmail.com>
2021-03-25 14:38:50 -0400

    commit tree 0dfb66c52166269e057be9a50b54a86cbbf09a30

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,17 @@
+class Course
+    attr_accessor :title, :schedule, :description
+    @@all = []
 
+    def initialize 
+        @@all << self
+    end
+    # Class Methods
+    def self.reset_all
+        @@all.clear
+    end
 
+    def self.all
+        @@all
+    end
+    
+end
